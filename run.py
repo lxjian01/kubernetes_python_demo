30332051128e552358ea30839873ca1f0a8fc29f
lij138 <lij138@bngrp.com>
2021-04-20 04:07:05 -0400

    yaml

---
+++ run.py
@@ -0,0 +1,33 @@
+from os import path
+
+import yaml
+from kubernetes import client, config, watch
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
+    dep = yaml.safe_load(f)
+    k8s_apps_v1 = client.AppsV1Api()
+    resp = k8s_apps_v1.create_namespaced_deployment(
+        body=dep, namespace="default")
+    print("Deployment created. status='%s'" % resp.metadata.name)
+
+count = 10
+w = watch.Watch()
+for event in w.stream(v1.list_namespace, _request_timeout=60):
+    print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+    count -= 1
+    if not count:
+        w.stop()
+
+print("Ended.")
+
+
+
